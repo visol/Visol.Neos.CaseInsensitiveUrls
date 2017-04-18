73243d65f4810848d3b0da29acfe39da2fd114cb
Peter Bucher <peter.bucher@bellicon.com>
2017-04-18 16:21:00 +0200

    Manual migration to Neos 3.0. Added missed renaming

--- Classes/Swisscom/Neos/CaseInsensitiveUrls/Migration/Transformations/ChangePropertyValueToLowercase.php
+++ Classes/Swisscom/Neos/CaseInsensitiveUrls/Migration/Transformations/ChangePropertyValueToLowercase.php
@@ -7,8 +7,8 @@ namespace Swisscom\Neos\CaseInsensitiveUrls\Migration\Transformations;
  *                                                                        *
  *                                                                        */
 
-use TYPO3\Flow\Annotations as Flow;
-use TYPO3\TYPO3CR\Migration\Transformations\AbstractTransformation;
+use Neos\Flow\Annotations as Flow;
+use Neos\ContentRepository\Migration\Transformations\AbstractTransformation;
 
 /**
  * Change the value of a given property to lowercase.
@@ -33,20 +33,20 @@ class ChangePropertyValueToLowercase extends AbstractTransformation {
 	 * If the given node has the property this transformation should work on, this
 	 * returns TRUE.
 	 *
-	 * @param \TYPO3\TYPO3CR\Domain\Model\NodeData $node
+	 * @param \Neos\ContentRepository\Domain\Model\NodeData $node
 	 * @return boolean
 	 */
-	public function isTransformable(\TYPO3\TYPO3CR\Domain\Model\NodeData $node) {
+	public function isTransformable(\Neos\ContentRepository\Domain\Model\NodeData $node) {
 		return ($node->hasProperty($this->propertyName));
 	}
 
 	/**
 	 * Change the property on the given node.
 	 *
-	 * @param \TYPO3\TYPO3CR\Domain\Model\NodeData $node
+	 * @param \Neos\√\Domain\Model\NodeData $node
 	 * @return void
 	 */
-	public function execute(\TYPO3\TYPO3CR\Domain\Model\NodeData $node) {
+	public function execute(\Neos\ContentRepository\Domain\Model\NodeData $node) {
 		$currentPropertyValue = $node->getProperty($this->propertyName);
 		$newPropertyValue = strtolower($currentPropertyValue);
 		$node->setProperty($this->propertyName, $newPropertyValue);
